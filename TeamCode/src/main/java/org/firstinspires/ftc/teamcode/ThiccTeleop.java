9f3b2d89998c7e3175afc4cc9dffaae2a79f5976
Andy He <xia.yifu82@gmail.com>
2018-06-12 20:06:23 -0400

    started to integrate the servos and motors into the teleop programme // also testing sourcetree

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThiccTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThiccTeleop.java
@@ -3,20 +3,55 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Teleop")
+@TeleOp(name = "Teleop")
 public class ThiccTeleop extends OpMode {
     HolonomicDrivebase drivebase;
 
     DcMotor mtrIntakeLeft;
     DcMotor mtrIntakeRight;
 
+    DcMotor mtrGlyphLift;
+
+    DcMotor mtrRelic;
+
+    Servo svoJewelPivot;
+    Servo svoJewelLift;
+
+    Servo svoRelicPivot;
+    Servo svoRelicGrab;
+
+    Servo svoFlipperFlip;
+    Servo svoFlipperTop;
+    Servo svoFlipperBottom;
+
+    Servo svoAutoLeft;
+    Servo svoAutoRight;
+
     @Override
     public void init() {
         drivebase = new HolonomicDrivebase(hardwareMap);
 
-        mtrIntakeLeft = hardwareMap.dcMotor.get("inL");
-        mtrIntakeRight = hardwareMap.dcMotor.get("inR");
+        mtrIntakeLeft = hardwareMap.dcMotor.get("mtrIntakeLeft");
+        mtrIntakeRight = hardwareMap.dcMotor.get("mtrIntakeRight");
+
+        mtrGlyphLift = hardwareMap.dcMotor.get("mtrGlyphLift");
+        mtrRelic = hardwareMap.dcMotor.get("mtrRelic");
+
+        svoAutoLeft = hardwareMap.servo.get("svoAutoLeft");
+        svoAutoRight = hardwareMap.servo.get("svoAutoRight");
+
+        svoJewelLift = hardwareMap.servo.get("svoJewelLift");
+        svoJewelPivot = hardwareMap.servo.get("svoJewelPivot");
+
+        svoFlipperBottom = hardwareMap.servo.get("svoFlipperBottom");
+        svoFlipperFlip = hardwareMap.servo.get("svoFlipperFlip");
+
+        svoFlipperTop = hardwareMap.servo.get("svoFlipperTop");
+        svoRelicGrab = hardwareMap.servo.get("svoRelicGrab");
+        svoRelicPivot = hardwareMap.servo.get("svoRelicPivot");
+
     }
 
     @Override
@@ -29,7 +64,6 @@ public class ThiccTeleop extends OpMode {
         double x2 = Math.copySign(Math.pow(gamepad1.right_stick_x, 2), gamepad1.right_stick_x);
 
         drivebase.driveArcade(x1, y1, x2, 1);
-
 
 
         // ----- intake ------
